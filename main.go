9bc601cb7e06d23290ba8aa7207cd9125a8f22f9
ToastedGMS <gabrielsiqueira3052@gmail.com>
2026-02-25 14:08:07 -0300

    feat: implement goroutines to fetch all stories simultaneously

--- main.go
+++ main.go
@@ -1,8 +1,47 @@
 package main
 
-type Article struct {
-	Title, URL, Img, Date, Source string
+import (
+	"fmt"
+	"os"
+	"sync"
+
+	"github.com/ToastedGMS/go-scraper/sources"
+	"github.com/ToastedGMS/go-scraper/types"
+)
+
+func RunScrapers(query string) []types.Article {
+	var stack sync.WaitGroup
+	stack.Add(3)
+	channel := make(chan types.Article, 3)
+
+	go func() {
+		defer stack.Done()
+		channel <- sources.G1(query)
+	}()
+
+	go func() {
+		defer stack.Done()
+		channel <- sources.Cnn(query)
+	}()
+
+	go func() {
+		defer stack.Done()
+		channel <- sources.Metro(query)
+	}()
+
+	go func() {
+		stack.Wait()
+		close(channel)
+	}()
+
+	var results []types.Article
+
+	for item := range channel {
+		results = append(results, item)
+	}
+	return results
 }
 
 func main() {
+	fmt.Println(RunScrapers(os.Args[1]))
 }
