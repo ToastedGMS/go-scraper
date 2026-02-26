59f85eb78ab9cd072c3b4ea74cdc40c7dbf6d6d7
ToastedGMS <gabrielsiqueira3052@gmail.com>
2026-02-26 20:56:21 -0300

    refactor: update RunScrapers to return errors alongside articles

--- main.go
+++ main.go
@@ -9,37 +9,48 @@ import (
 	"github.com/ToastedGMS/go-scraper/types"
 )
 
-func RunScrapers(query string) []types.Article {
+func RunScrapers(query string) ([]types.Article, []error) {
 	var stack sync.WaitGroup
 	stack.Add(3)
-	channel := make(chan types.Article, 3)
 
-	go func() {
-		defer stack.Done()
-		channel <- sources.G1(query)
-	}()
+	type Results struct {
+		article types.Article
+		errors  error
+	}
 
-	go func() {
-		defer stack.Done()
-		channel <- sources.Cnn(query)
-	}()
+	responseChannel := make(chan Results, 3)
 
-	go func() {
-		defer stack.Done()
-		channel <- sources.Metro(query)
-	}()
+	functions := []func(string) (types.Article, error){
+		sources.Cnn,
+		sources.G1,
+		sources.Metro,
+	}
+
+	for _, function := range functions {
+		go func(f func(string) (types.Article, error)) {
+			defer stack.Done()
+			article, errors := f(query)
+			responseChannel <- Results{article, errors}
+		}(function)
+	}
 
 	go func() {
 		stack.Wait()
-		close(channel)
+		close(responseChannel)
 	}()
 
 	var results []types.Article
+	var issues []error
 
-	for item := range channel {
-		results = append(results, item)
+	for result := range responseChannel {
+		if result.errors != nil {
+			issues = append(issues, result.errors)
+			continue
+		}
+		results = append(results, result.article)
 	}
-	return results
+
+	return results, issues
 }
 
 func main() {
