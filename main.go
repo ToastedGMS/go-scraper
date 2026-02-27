1ddcc8134d5cd1e71f417480b7d2c1a6c484d235
ToastedGMS <gabrielsiqueira3052@gmail.com>
2026-02-27 14:36:15 -0300

    feat: implement encapsulation of similar news articles -group articles by comparing titles and order them by publishing date

--- main.go
+++ main.go
@@ -4,7 +4,11 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
+	"sort"
+	"strings"
 	"sync"
+	"unicode"
+	"unicode/utf8"
 
 	"github.com/ToastedGMS/go-scraper/sources"
 	"github.com/ToastedGMS/go-scraper/types"
@@ -76,9 +80,100 @@ func ScraperHandler(w http.ResponseWriter, r *http.Request) {
 		json.NewEncoder(w).Encode(map[string]interface{}{"error": "Failed to fetch data"})
 		return
 	}
+
+	sort.Slice(articles, func(i, j int) bool {
+		return articles[i].Date > articles[j].Date
+	})
+
+	var stories = GroupArticles(articles)
+
+	sort.Slice(stories, func(i, j int) bool {
+		return len(stories[i].Articles) > len(stories[j].Articles)
+	})
 	encoder := json.NewEncoder(w)
 	encoder.SetIndent("", " ")
-	encoder.Encode(articles)
+	encoder.Encode(stories)
+}
+
+func processString(val string) []string {
+	val = strings.ToLower(val)
+	f := func(r rune) rune {
+		if unicode.IsPunct(r) || unicode.IsSymbol(r) {
+			return -1
+		}
+		return r
+	}
+	val = strings.Map(f, val)
+
+	stringSlice := strings.Split(val, " ")
+
+	var filteredStringSlice []string
+	for _, item := range stringSlice {
+		item = strings.TrimSpace(item)
+		if utf8.RuneCountInString(item) >= 4 {
+			filteredStringSlice = append(filteredStringSlice, item)
+		}
+	}
+	return filteredStringSlice
+}
+
+func countEqualStrings(slice1, slice2 []string) int {
+	count := 0
+
+	for _, word := range slice1 {
+		for _, word2 := range slice2 {
+			if word == word2 {
+				count++
+				break
+			}
+		}
+	}
+	return count
+}
+
+func GroupArticles(articles []types.Article) []types.Story {
+	ArticleMap := make(map[int]bool)
+	var stories []types.Story
+
+	for i := 0; i < len(articles); i++ {
+		ArticleMap[i] = false
+	}
+
+	for i := 0; i < len(articles); i++ {
+		if ArticleMap[i] == true {
+			continue
+		}
+		var story types.Story
+
+		var longestTitle string
+		story.Headline = articles[i].Title
+		story.Articles = append(story.Articles, articles[i])
+		ArticleMap[i] = true
+
+		for j := 0; j < len(articles); j++ {
+			if ArticleMap[j] == true {
+				continue
+			}
+
+			for _, article := range story.Articles {
+				if countEqualStrings(processString(article.Title), processString(articles[j].Title)) >= 3 {
+					story.Articles = append(story.Articles, articles[j])
+					ArticleMap[j] = true
+					break
+				}
+			}
+		}
+
+		for _, article := range story.Articles {
+			if utf8.RuneCountInString(article.Title) > utf8.RuneCountInString(longestTitle) {
+				longestTitle = article.Title
+			}
+			story.Headline = longestTitle
+		}
+		stories = append(stories, story)
+
+	}
+	return stories
 }
 
 func main() {
